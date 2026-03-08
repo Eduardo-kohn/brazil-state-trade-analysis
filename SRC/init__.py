088396e936ca6495eefb87091f05aa962f02826e
Eduardo Kohn <67090583+Eduardo-kohn@users.noreply.github.com>
2026-03-07 21:52:38 -0300

    Update init__.py

--- SRC/init__.py
+++ SRC/init__.py
@@ -3,3 +3,7 @@ import psycopg2
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 from dotenv import load_dotenv
 import os
+import re
+import shutil
+from datetime import datetime
+from sqlalchemy import create_engine
